6e34819ea5296229cba8ce7fa66135ae98df1619
DanendraGafrila25 <123918948+DanendraGafrila25@users.noreply.github.com>
2023-05-03 23:44:33 +0700

    Update huff_body.c

--- huff_body.c
+++ huff_body.c
@@ -45,41 +45,86 @@ char* read_file(char* input_file) {
         printf("File tidak ditemukan\n");
         return;
     }
-	fscanf(file, "%s", text);
-//    printf("\n\tIsi file %s:\n", input_file);
-//    printf("\t--------------------\n");
-//
-//    while ( == 1) {
-//        printf("\t%s ", teks);
-//    }
-//
-//    printf("\n\t---------------------\n");
+	fscanf(file, "%[^\n]s", text);
 	return text;
     fclose(file);
 }
 
 Taddres Build_Huffman(ListQueue *L){
-	Taddres root;
-	Taddres n1;
-	Taddres n2;
-	
-	while (L->size > 1)
-	{
-		// Get first smallest node  
-		n1 = peek(*L);
-		//Remove a front element
-		Deque(&(*L));
-		// Get second smallest node
-		n2 = peek(*L);
-		// Remove a front element
-		Deque(&(*L));
-		// Make new node using two smallest node
-		//root =  newTreeNode(n1->freq + n2->freq, ' ');
-		// Add new node into priority queue 
-		Insrt_Node(&(*L),n1->freq + n2->freq,' ');
-		// Set left and right child
-		root->LSon = n1;
-		root->RSon = n2;
-	}
-	return root;
+    Taddres root;
+    Taddres n1;
+    Taddres n2;
+
+    while (L->size > 1)
+    {
+        // Get first smallest node  
+        n1 = peek(*L);
+        // Remove a front element
+        Deque(&(*L));
+        // Get second smallest node
+        n2 = peek(*L);
+        // Remove a front element
+        Deque(&(*L));
+        // Make new node using two smallest node
+        root = Create_TNode(n1->freq + n2->freq, ' ');
+        // Set left and right child
+        root->LSon = n1;
+        root->RSon = n2;
+        // Add new node into priority queue 
+        enque(&(*L),root); 
+		//Insrt_Node(&(*L), root->freq, root->info);
+    }
+    return root;
 }
+
+
+
+// Display Huffman code
+
+void PrintHuffman(Taddres node, char result[], int n, encoding tamp[],int x)
+{
+    if (node == NULL)
+    {
+        return;
+    }
+    if (node->LSon == NULL && node->RSon == NULL)
+    {
+        result[n] = '\0';
+        tamp[x].info = node->info;
+        strcpy(tamp[x].value,result);
+        printf("\n\t %c = %s", node->info, result);
+        x += 1;
+        return;
+    }
+    result[n] = '0';
+    PrintHuffman(node->LSon, result, n + 1, tamp, x);
+    result[n] = '1';
+    PrintHuffman(node->RSon, result, n + 1, tamp, x);
+}
+
+
+void PrintHuffmanHelper(Taddres root,int max_tree){
+	
+	
+    char result[max_tree]; // inisialisasi array result
+    encoding tamp[max_tree]; // inisialisasi array tamp
+    int n = 0, x = 0; // inisialisasi variabel n dan x
+
+    PrintHuffman(root, result, n, tamp, x); // panggil fungsi rekursif helper
+
+}
+
+void PrintTree(Taddres root, int level) {
+    if (root == NULL) {
+        return;
+    }
+    PrintTree(root->RSon, level + 1);
+    int i;
+    for ( i = 0; i < level; i++) {
+        printf("    ");
+    }
+    printf("%c:%d\n", root->info, root->freq);
+    PrintTree(root->LSon, level + 1);
+}
+
+
